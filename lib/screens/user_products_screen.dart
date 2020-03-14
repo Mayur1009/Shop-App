b5ee34f9f3a995f25de17e01c9898c42fdbed04f
Mayur1009 <mayur.k.shende@gmail.com>
2020-03-14 22:23:05 +0530

    Added User Product Screen

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/user_products_item.dart';
+import '../widgets/app_drawer.dart';
+import '../providers/products.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-product';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: const Text('Your products'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => Column(
+            children: <Widget>[
+              UserProductItem(
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../screens/orders_screen.dart';
+import '../screens/user_products_screen.dart';
+
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,22 +15,32 @@ class AppDrawer extends StatelessWidget {
             ),
             automaticallyImplyLeading: false,
           ),
-	        Divider(),
-	        ListTile(
-		        leading: Icon(Icons.shop),
-		        title: Text('Shop'),
-		        onTap: () {
-		        	Navigator.of(context).pushReplacementNamed('/');
-		        },
-	        ),
-	        Divider(),
-	        ListTile(
-		        leading: Icon(Icons.payment),
-		        title: Text('Orders'),
-		        onTap: () {
-			        Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName);
-		        },
-	        ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Shop'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text('Orders'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(OrdersScreen.routeName);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Manage Products'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(UserProductsScreen.routeName);
+            },
+          ),
         ],
       ),
     );
