e7b5abdb0516f78b23d88a71c74da1d4d5b5d32e
Mayur1009 <mayur.k.shende@gmail.com>
2020-03-16 11:47:10 +0530

    done http requests module

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -271,11 +271,6 @@ class _EditProductScreenState extends State<EditProductScreen> {
                                   !value.startsWith('https')) {
                                 return 'Please enter a valid URL.';
                               }
-                              if (!value.endsWith('.png') &&
-                                  !value.endsWith('.jpg') &&
-                                  !value.endsWith('.jpeg')) {
-                                return 'Please enter a valid image URL.';
-                              }
                               return null;
                             },
                             onSaved: (value) {
